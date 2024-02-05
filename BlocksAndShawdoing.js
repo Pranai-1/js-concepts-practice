4d0adf5286cc5969a90e70956defb9f179dd81d2
Pranai-1 <reddypranai2017@gmail.com>
2024-02-05 16:09:55 +0530

    shadowing

--- BlocksAndShawdoing.js
+++ BlocksAndShawdoing.js
@@ -10,6 +10,7 @@ if(true)true
 var c=100
 let a=10;
 {
+    
      let a=5;      
     const b=10;  //here a and b are hoisted and placed in a block scoped memory but c is placed in global scope memory
     var c=20;//var is function scoped so we can access it outside of this block as it is present in global scope in this case
@@ -51,3 +52,31 @@ console.log(s)
 //s have different scopes 
 //first s is present in script scope
 //second s is present in block scope and third s also in block scope but in a different block
+
+
+
+// Creating a symbol with an optional description
+const mySymbol = Symbol('This is a symbol');
+
+// Symbols are guaranteed to be unique
+const anotherSymbol = Symbol('This is a symbol');
+
+console.log(mySymbol === anotherSymbol); // Outputs: false (symbols are unique)
+
+// Symbols can be used as property keys
+const obj = {
+  [mySymbol]: 'Symbol as a property key'
+};
+
+console.log(obj); // Outputs: {Symbol(This is a symbol): 'Symbol as a property key'}
+
+const obj1={
+    key1:10
+}
+
+const key2='hey'
+const obj2={
+    [key2]:10
+}
+console.log(obj1)
+console.log(obj2)
