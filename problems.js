88cabc9187b20e4f8bb12ddebbcb51aaa7ba8d1d
Pranai-1 <reddypranai2017@gmail.com>
2024-02-14 12:03:01 +0530

    added a problem

--- problems.js
+++ problems.js
@@ -11,3 +11,71 @@ let sum=function(a){
 }
 
 console.log(sum(1)(2)(3)(4)(5)())
+
+// let obj = {
+//     foo: "pranai",
+//     fnc: function () {
+//         var self = this;//self points to obj
+//        // console.log(this)//this points to obj
+//         console.log(this.foo); // Outputs: "pranai"
+//         console.log(self.foo); // Outputs: "pranai"
+//         (function () {
+//            // console.log(this)//this points to global object
+//             //console.log(self)//self points to obj
+//             console.log(this.foo); // undefined (or throws an error in strict mode)
+//             console.log(self.foo); // Outputs: "pranai"
+//         })()  //use .bind(this) to bind with current context
+//     }
+// }
+// obj.fnc();
+
+// let obj2 = {
+//     foo: "pranai",
+//     fnc: function () {
+//         var self = this;//self points to obj
+//         console.log(this)//this points to obj
+//         console.log(this.foo); // Outputs: "pranai"
+//         console.log(self.foo); // Outputs: "pranai"
+//         ( ()=> {//arrow function takes surrounding context which is the context of fnxc
+//             console.log(this)//this points to global object
+//             console.log(self)//self points to obj
+//             console.log(this.foo); // Outputs: "pranai"
+//             console.log(self.foo); // Outputs: "pranai"
+//         })()  
+//     }
+// }
+// /obj2.fnc();
+
+// let obj3 = {
+//     foo: "pranai",
+//     fnc:  ()=> {
+//         var self = this;//self points to global
+//         console.log(this)//this points to global
+//         console.log(this.foo); // Outputs: undefined
+//         console.log(self.foo); // Outputs: undefined
+//         ( ()=> {//arrow function takes surrounding context which is the context of fnxc
+//             console.log(this)//this points to global object
+//             console.log(self)//self points to global
+//             console.log(this.foo); // Outputs: undefined
+//             console.log(self.foo); // Outputs: undefined
+//         })()  
+//     }
+// }
+// obj3.fnc();
+
+let obj4 = {
+    foo: "pranai",
+    fnc:  ()=> {
+        var self = this;//self points to global
+        console.log(this)//this points to global
+        console.log(this.foo); // Outputs: undefined
+        console.log(self.foo); // Outputs: undefined
+        ( function() {//arrow function takes surrounding context which is the context of fnxc
+            console.log(this)//this points to global object
+            console.log(self)//self points to global
+            console.log(this.foo); // Outputs: undefined
+            console.log(self.foo); // Outputs: undefined
+        })()  
+    }
+}
+obj4.fnc();
