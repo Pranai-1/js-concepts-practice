581521f9563e18bd45ed6a053dfa6ebd8b72cf87
Pranai-1 <reddypranai2017@gmail.com>
2024-02-26 14:19:03 +0530

    changes

--- problems.js
+++ problems.js
@@ -2,26 +2,26 @@
 //This function will return a final output whenever there is an empty parameter 
 //This function will return another function if there are any parameters
 
-let sum=function(a){
-    return function(b){
-        if(b)
-        return sum(a+b)
-       return a
-    }
-}
+// let sum=function(a){
+//     return function(b){
+//         if(b)
+//         return sum(a+b)
+//        return a
+//     }
+// }
 
-console.log(sum(1)(2)(3)(4)(5)())
+// console.log(sum(1)(2)(3)(4)(5)())
 
 // let obj = {
 //     foo: "pranai",
 //     fnc: function () {
 //         var self = this;//self points to obj
-//        // console.log(this)//this points to obj
+//        console.log(this)//this points to obj
 //         console.log(this.foo); // Outputs: "pranai"
 //         console.log(self.foo); // Outputs: "pranai"
 //         (function () {
-//            // console.log(this)//this points to global object
-//             //console.log(self)//self points to obj
+//             console.log(this)//this points to global object
+//             console.log(self)//self points to obj
 //             console.log(this.foo); // undefined (or throws an error in strict mode)
 //             console.log(self.foo); // Outputs: "pranai"
 //         })()  //use .bind(this) to bind with current context
@@ -63,20 +63,33 @@ console.log(sum(1)(2)(3)(4)(5)())
 // }
 // obj3.fnc();
 
-let obj4 = {
-    foo: "pranai",
-    fnc:  ()=> {
-        var self = this;//self points to global
-        console.log(this)//this points to global
-        console.log(this.foo); // Outputs: undefined
-        console.log(self.foo); // Outputs: undefined
-        ( function() {//arrow function takes surrounding context which is the context of fnxc
-            console.log(this)//this points to global object
-            console.log(self)//self points to global
-            console.log(this.foo); // Outputs: undefined
-            console.log(self.foo); // Outputs: undefined
-        })()  
-    }
+// let obj4 = {
+//     foo: "pranai",
+//     fnc:  ()=> {
+//         var self = this;//self points to global
+//         console.log(this)//this points to global
+//         console.log(this.foo); // Outputs: undefined
+//         console.log(self.foo); // Outputs: undefined
+//         ( function() {//arrow function takes surrounding context which is the context of fnxc
+//             console.log(this)//this points to global object
+//             console.log(self)//self points to global
+//             console.log(this.foo); // Outputs: undefined
+//             console.log(self.foo); // Outputs: undefined
+//         })()  
+//     }
+// }
+// obj4.fnc();
+// localStorage.getItem("x")
+
+function wait(){
+    console.log("inside wait")//1st
+    setTimeout(()=>{  console.log("waited for 1 ms")},1)//4th eventhough it is 1ms
+    console.log("completed wait")//2nd
 }
-obj4.fnc();
-localStorage.getItem("x")
+function noWait(){
+    console.log("inside noWait")//3rd
+    
+}
+wait()
+noWait()
+//This means that after wait() function synchronous operations are finished it will be removed from the stack
