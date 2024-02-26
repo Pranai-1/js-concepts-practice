e91e115319f70a95b8dbac41a6f69129da7b853f
Pranai-1 <reddypranai2017@gmail.com>
2024-02-26 18:11:20 +0530

    changes

--- constructor.js
+++ constructor.js
@@ -25,12 +25,11 @@ Object.prototype.divide=function(){
 //  console.log(obj.constructor)
 //  console.log(str.constructor)
 //  console.log(x.constructor)
- //console.log(x.divide())
+ console.log(x.divide())//present
 //Adding properties to a  function constructor
  function Person(name) {
     this.name = name;}
   
-  const person1 = new Person("Alice");
   person1.greet ="hi"  //same as person1[‘greet]=”hi”
   const person2 = new Person("Bob");
   console.log(person2)// Person { name: 'Bob' }
